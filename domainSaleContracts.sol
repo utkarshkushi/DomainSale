931625c8364e37828b69919adef98730be6a6616
Utkarsh Suneela <77976877+utkarshkushi@users.noreply.github.com>
2023-05-03 03:19:47 +0530

    updating the contract with necessary changes

--- domainSaleContracts.sol
+++ domainSaleContracts.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // A sample contract issuing ERC721 tokens 
@@ -44,35 +44,31 @@ contract MyNFT is ERC721URIStorage {
 
 // Domain sale Contract
 
-contract domainSales {
+contract domainSales is Ownable {
     
-    address payable public owner;
+    address payable public contractOwner;
     uint8 private maxDiscountPercentAge = 20; 
     uint public salePrice;
-    uint8 private refferalDiscount = 10; 
+    uint8 private defaultRefferalDiscount = 10; 
     
-    // upadatinf when someone buys the domain 
+    // upadating when someone buys the domain 
     mapping(string => bool) public DomainsRegistered;
     mapping(string => address) public DomainOwningAddress;
     
-    // this is for refferal system
-    uint private refferalId = 1;
-    mapping(uint256 => address) private refferedAddress;
-    //this is public to know the refferal ID
-    mapping(address => uint) public refferalIdOfRefferedAddress;
-    mapping(uint256 => address) private refferalGivenBy;
+    //event 
+    event DomainNamePurchased(
+        address indexed purchasedBy,
+        string indexed domainNamePurchased
+    );
 
-    //checking if refferal id already used 
-    mapping(uint256 => bool) private refferalIdUsed;
+    //whiteListing users for 20% discount 
+    mapping(address => bool) public whiteListedAddresses;
 
-    modifier onlyOwner{
-        require(msg.sender == owner, "only owner can call this function");
-        _;
-    }
+    //re-entrancy variable
+    bool transactionInProgress;
 
     constructor(){
-        owner = payable(msg.sender);
-        // deployedContractAddress = _contractAddress;
+        contractOwner = payable(msg.sender);
     }
 
     //creating a instance of 
@@ -85,13 +81,21 @@ contract domainSales {
 
     //withdrawing any eth in the contract
     function withdraw() external onlyOwner{
-        owner.transfer(address(this).balance);
+        require(transactionInProgress == false, "a transaction is being processed");
+        transactionInProgress = true;
+        contractOwner.transfer(address(this).balance);
     }
 
     //setting refferal discount
-    function setRefferalDiscount(uint8 _discount) external onlyOwner{
-        refferalDiscount = _discount;
+    function setDefaultRefferalDiscount(uint8 _discount) external onlyOwner{
+        require(_discount <= 20, "default refferal cannot go above 20 %");
+        defaultRefferalDiscount = _discount;
     } 
+
+    //whitlisting custom refferal discounts
+    function addAddressToWhiteList(address _whiteListingAddress) external onlyOwner {
+        whiteListedAddresses[_whiteListingAddress] = true;
+    }
 
     //checking if domain available for sale
     function checkDomainAvailability(string memory domainName) public view returns(bool){
@@ -106,6 +110,8 @@ contract domainSales {
 
     // buying the doamain without refferal and msg.value sent to contrcat owner directly
     function buyDomain(address _to, string memory _domainName) external payable{
+        require(transactionInProgress == false, "a transaction is being processed");
+        transactionInProgress = true;
         require(!checkDomainAvailability(_domainName), "Domain already registered");
         require(_to != address(0), "not a valid address");
         require(msg.value == salePrice, "you haven't sent enough eth");
@@ -113,14 +119,17 @@ contract domainSales {
         myNft.mintNFT(_to, _domainName);
         DomainsRegistered[_domainName] = true;
         DomainOwningAddress[_domainName] = _to;
-        owner.transfer(msg.value);
+        contractOwner.transfer(msg.value);
+
+        emit DomainNamePurchased(_to, _domainName);
     }
 
     //buying domain with refferal and paying refferal amount to the one who gave the refferal
-    function  buyDomainRefferal(address _to,string memory _domainName, uint256 _refferralId) external payable{
+    function  buyDomainRefferal(address _to,string memory _domainName, string memory _refferedBy) external payable{
+        require(transactionInProgress == false, "a transaction is being processed");
+        transactionInProgress = true;
         require(!checkDomainAvailability(_domainName), "Domain already registered");
-        require(refferedAddress[_refferralId] != address(0), "not a valid referral address");
-        require(refferalIdUsed[_refferralId] == false, "refferal Id already used");
+        require(DomainsRegistered[_refferedBy] == true, "your are not elibilbe to give refferal");
         require(_to != address(0), "not a valid address");
         require(msg.value == salePrice, "you haven't sent enough eth");
 
@@ -128,42 +137,41 @@ contract domainSales {
         DomainsRegistered[_domainName] = true;
         DomainOwningAddress[_domainName] = _to;
 
-        refferalIdUsed[_refferralId] = true;
-
-        uint256 refferalAmount = (msg.value * refferalDiscount ) / 100; 
-        payable(refferalGivenBy[_refferralId]).transfer(refferalAmount);
-        owner.transfer(msg.value - refferalAmount);
-
+       if(whiteListedAddresses[DomainOwningAddress[_refferedBy]] == true){
+           makePaymentThroughRefferal(20, msg.value, DomainOwningAddress[_refferedBy]);
+           emit DomainNamePurchased(_to, _domainName);
+       }
+       else{
+           makePaymentThroughRefferal(defaultRefferalDiscount, msg.value, DomainOwningAddress[_refferedBy]);
+           emit DomainNamePurchased(_to, _domainName);
+       }
     } 
+
+    function makePaymentThroughRefferal(uint _discountPercent, uint _sentETH, address _refferedBy) private {
+        uint256 refferalAmount = (_sentETH * _discountPercent ) / 100; 
+            payable(_refferedBy).transfer(refferalAmount);
+            contractOwner.transfer(_sentETH - refferalAmount);
+    }
 
     
     //reserving doamins
     function reserveDomainsOnlyOwner(address _to, string memory _domainName) external onlyOwner {
+        require(transactionInProgress == false, "a transaction is being processed");
+        transactionInProgress = true;
         require(!checkDomainAvailability(_domainName), "Domain already registered");
         require(_to == address(0), "not a valid address");
         myNft.mintNFT(_to, _domainName);
         DomainsRegistered[_domainName] = true;
         DomainOwningAddress[_domainName] = _to;
+        emit DomainNamePurchased(_to, _domainName);
     }
 
-
-    //giving refferal
-    function giveRefferal(string memory _existingDomain, address _refferingTo) external {
-        require(DomainOwningAddress[_existingDomain] != address(0), "you dont have a domain to reffer others");
-        require(msg.sender == DomainOwningAddress[_existingDomain], "you are not allowed to give refferal");
-        refferedAddress[refferalId] = _refferingTo;
-        refferalIdOfRefferedAddress[_refferingTo] = refferalId;
-        refferalGivenBy[refferalId] = DomainOwningAddress[_existingDomain];
-        refferalIdUsed[refferalId] = false;
-        refferalId++;
-
-    }
 
     //function to remove unwated ERC20 tokens
     function removeUnwantedTokens(address _tokenAddress) external onlyOwner{
         ERC20TokenContract erc20Tokens = ERC20TokenContract(_tokenAddress);
         uint bal = erc20Tokens.balanceOf(address(this));
-        erc20Tokens.transfer(owner, bal);
+        erc20Tokens.transfer(contractOwner, bal);
     }
 
 }
